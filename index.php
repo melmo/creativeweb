bad90c2951161104c4f7d39395589377b46b4a72
jonas-halbach <jonas@jonas-halbach.de>
2016-11-02 19:23:40 +0100

    added lesson 3 to index.html

--- index.php
+++ index.php
@@ -32,14 +32,32 @@
         <div class="row">
             <div class="col-md-7">
                 <a href="#">
-                    <img class="img-responsive" src="demo/lesson-1/screenshot.png" alt="">
+                    <img class="img-responsive" src="demo/dismiss7Game/screenshot.png" alt="">
                 </a>
             </div>
             <div class="col-md-5">
-                <h3>Lesson 1</h3>
+                <h3>Lesson 3</h3>
                 <h4>JavaScript basics and an image slider</h4>
                 <p>In this lesson, we look at the basics of programming in Javascript and how to use it to access objects on a web page. Our first demo is a simple image slider that can be controlled with two buttons on the page.</p>
                 <a class="btn btn-primary" href="demo/lesson-1/demo.html">View Demo <span class="glyphicon glyphicon-chevron-right"></span></a> <a class="btn btn-primary" href="demo/lesson-1/lesson-1.zip">Download Demo <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+		
+		<!-- Project Two -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="demo/dismiss7Game/screenshot.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Lesson 3</h3>
+                <h4>Functions, Control Flow, Modulo and a little game</h4>
+                <p>In this lesson we continue practicing funtions and control flow (especially if, else and for-loops.). For practice we also create a little game we you count from 1 onwards, but are not allowed to print any numbers which are the result of a multiplication with 7.</p>
+                <a class="btn btn-primary" href="demo/dismiss7Game/index.html">View Demo <span class="glyphicon glyphicon-chevron-right"></span></a> <a class="btn btn-primary" href="demo/dismiss7Game/dismiss7Game.zip">Download Demo <span class="glyphicon glyphicon-chevron-right"></span></a>
             </div>
         </div>
         <!-- /.row -->
@@ -54,7 +72,7 @@
                 </a>
             </div>
             <div class="col-md-5">
-                <h3>Lesson 3</h3>
+                <h3>Lesson 4</h3>
                 <h4>Control flow and some games</h4>
                 <p>In this lesson we revise functions and control flow and use our skills to build some simple games.</p>
                 <a class="btn btn-primary" href="demo/lesson-3/index.html">View Demo <span class="glyphicon glyphicon-chevron-right"></span></a> <a class="btn btn-primary" href="demo/lesson-3/lesson-3.zip">Download Demo <span class="glyphicon glyphicon-chevron-right"></span></a>
