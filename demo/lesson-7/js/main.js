594eda90f976c3e9e51bf05eebd83f507552c92b
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2017-01-12 14:21:28 +0100

    update lesson 7 to include Lima info pane

--- demo/lesson-7/js/main.js
+++ demo/lesson-7/js/main.js
@@ -2,18 +2,26 @@ jQuery(document).ready(function($) {
 	var map = new GMaps({
 	  div: '#map',
 	  lat: -12.043333,
-	  lng: -77.028333
+	  lng: -77.028333,
+	  zoom: 10,
+	  mapType: 'satellite'
 	});
 
 	map.addMarker({
 	  lat: -12.043333,
 	  lng: -77.028333,
 	  title: 'Lima',
-	  click: function(e) {
-	    alert('You clicked in this marker');
+	  details : {
+	  	img : "https://upload.wikimedia.org/wikipedia/commons/e/e5/21_-_Lima_-_Ao%C3%BBt_2008.jpg",
+	  	city : "Lima"
 	  },
-	  infoWindow: {
-	  	content: '<p>HTML Content</p>'
+	  click: function(e) {
+	    //alert('You clicked in this marker');
+	    console.log(e.details.img);
+	    console.log(e.details.city);
+	    var html = '<h2>' + e.details.city + '</h2>';
+	    html += '<img src="' + e.details.img + '" width="100%">';
+	    $('#info').html(html);
 	  }
 	});
 });
